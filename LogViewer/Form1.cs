cb57017b20b9c95b3bb2f53b22876a06020984e2
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-03-14 23:25:07 +0100

    master new comment

--- LogViewer/Form1.cs
+++ LogViewer/Form1.cs
@@ -26,6 +26,7 @@ namespace LogViewer
               OpenConfigWindowEvent += maker.SetPropertiesOfTheWindow;
            // ChangeStateAndWindowHandler += maker.InvokeNewWindow;
           //  ChangeStateAndWindowHandler += maker.CreateNewWindow;
+          //sdsfdf
         }
         private void LoadLog_Click(object sender, EventArgs e)
         {
