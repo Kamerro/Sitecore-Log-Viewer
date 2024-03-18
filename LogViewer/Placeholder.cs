8a9dcd9db8f1fa2951410f317ce63d9804eeb53d
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-03-18 22:42:11 +0100

    master new list in placeholder

--- LogViewer/Placeholder.cs
+++ LogViewer/Placeholder.cs
@@ -5,11 +5,13 @@ namespace LogViewer
     internal class Placeholder
     {
         static internal List<string> List;
-        static internal List<string> ListErrorSolr;
-        static internal List<string> ListErrorUnicorn;
-        static internal List<string> ListWarnUnicorn;
+        static internal List<string> ListErrorsSolr;
+        static internal List<string> ListErrorsUnicorn;
+        static internal List<string> ListWarningsUnicorn;
         static internal List<string> ListWarningsSolr;
         static internal List<string> ListOfOtherWarns;
-        static internal List<string> ListSQL;
+        static internal List<string> ListErrorsSql;
+        static internal List<string> ListWarningsSql;
+
     }
 }
