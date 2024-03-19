6ea77f5623d5279b59652eb5c907ae1bd8272ffe
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-03-19 21:40:32 +0100

    master new confuguration class

---
+++ LogViewer/Configurations/OtherLogsConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogViewer.Configurations
+{
+    internal class OtherLogsConfigurations
+    {
+        public static string Sentence1 = String.Empty;
+        public static string Sentence2 = String.Empty;
+        public static bool Errors;
+        public static bool Warns;
+    }
+}
