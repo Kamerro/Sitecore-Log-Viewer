918dab7289d2473d711ef1cfa85d5fbc0c59c727
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-03-21 22:48:44 +0100

    master new class to spit more abstraction into algorithm

---
+++ LogViewer/Services/InfoLogs.cs
@@ -0,0 +1,99 @@
+﻿using System.Linq;
+using System;
+
+namespace LogViewer
+{
+    public class InfoLogs : IInfoLogs
+    {
+        private int _numberOfTheLines;
+        private int _numberOfErrors;
+        private int _numberOfSolrErrors;
+        private int _numberOfSolrWarns;
+        private int _numberOfWarns;
+        private int _numberOfInfos;
+        private int _numberOfDIWarnings;
+        private int _numberOfMixedErrors;
+        private int _numberOfSqlErrors;
+
+        public int NumberOfTheLines
+        {
+            get => _numberOfTheLines;
+            set
+            {
+                if (value is 0)
+                {
+                    throw new Exception("Empty Log file.");
+                }
+                _numberOfTheLines = value;
+            }
+        }
+        public int NumberOfErrors
+        {
+            get => _numberOfErrors;
+            set => _numberOfErrors = value;
+        }
+        public int NumberOfSolrErrors
+        {
+            get => _numberOfSolrErrors;
+            set => _numberOfSolrErrors = value;
+        }
+        public int NumberOfSolrWarns
+        {
+            get => _numberOfSolrWarns;
+            set => _numberOfSolrWarns = value;
+        }
+        public int NumberOfWarns
+        {
+            get => _numberOfWarns;
+            set => _numberOfWarns = value;
+        }
+        public int NumberOfInfos
+        {
+            get => _numberOfInfos;
+            set => _numberOfInfos = value;
+        }
+        public int NumberOfDIWarnings
+        {
+            get => _numberOfDIWarnings;
+            set => _numberOfDIWarnings = value;
+        }
+        public int NumberOfMixedErrors
+        {
+            get => _numberOfMixedErrors;
+            set => _numberOfMixedErrors = value;
+        }
+        public int NumberOfSqlErrors
+        {
+            get => _numberOfSqlErrors;
+            set => _numberOfSqlErrors = value;
+        }
+
+        public void CheckParametersOfTheProvidedLog(string fileContent)
+        {
+
+            string[] splitedContent = fileContent.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            _numberOfTheLines = splitedContent.Length;
+            _numberOfInfos = splitedContent.Count(x => x.ToLower().Contains("info"));
+            _numberOfWarns = splitedContent.Count(x => x.ToLower().Contains("warn"));
+            _numberOfErrors = splitedContent.Count(x => x.ToLower().Contains("error"));
+            _numberOfSolrErrors = splitedContent.Count(x => x.ToLower().Contains("error") && x.ToLower().Contains("solr"));
+            _numberOfSolrWarns = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("solr"));
+            _numberOfDIWarnings = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("constructor"));
+            _numberOfMixedErrors = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("error"));
+            _numberOfMixedErrors += splitedContent.Count(x => x.ToLower().Contains("info") && x.ToLower().Contains("error"));
+            _numberOfMixedErrors += splitedContent.Count(x => x.ToLower().Contains("info") && x.ToLower().Contains("warn"));
+            _numberOfSqlErrors += splitedContent.Count(x => x.ToLower().Contains("error") && x.ToLower().Contains("warn"));
+        }
+        public void CheckParametersOfTheProvidedLog(string[] splitedContent)
+        {
+            _numberOfTheLines = splitedContent.Length;
+            _numberOfInfos = splitedContent.Count(x => x.ToLower().Contains("info"));
+            _numberOfWarns = splitedContent.Count(x => x.ToLower().Contains("warn"));
+            _numberOfErrors = splitedContent.Count(x => x.ToLower().Contains("error"));
+            _numberOfSolrErrors = splitedContent.Count(x => x.ToLower().Contains("error") && x.ToLower().Contains("solr"));
+            _numberOfSolrWarns = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("solr"));
+            _numberOfDIWarnings = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("constructor"));
+        }
+
+    }
+}
--- LogViewer/Services/LogService.cs
+++ LogViewer/Services/LogService.cs
@@ -6,45 +6,8 @@ using System.Text;
 
 namespace LogViewer
 {
-    public class LogService
+    public class LogService:InfoLogs
     {
-        public int NumberOfTheLines;
-        public int NumberOfErrors;
-        public int NumberOfSolrErrors;
-        public int NumberOfSolrWarns;
-        public int NumberOfWarns;
-        public int NumberOfInfos;
-        public int NumberOfDIWarnings;
-        public int NumberOfMixedErrors;
-        public int NumberOfSqlErrors;
-
-        public void CheckParametersOfTheProvidedLog(string fileContent)
-        {
-
-            string[] splitedContent = fileContent.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-            NumberOfTheLines = splitedContent.Length;
-            NumberOfInfos = splitedContent.Count(x => x.ToLower().Contains("info"));
-            NumberOfWarns = splitedContent.Count(x => x.ToLower().Contains("warn"));
-            NumberOfErrors = splitedContent.Count(x => x.ToLower().Contains("error"));
-            NumberOfSolrErrors = splitedContent.Count(x => x.ToLower().Contains("error") && x.ToLower().Contains("solr"));
-            NumberOfSolrWarns = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("solr"));
-            NumberOfDIWarnings = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("constructor"));
-            NumberOfMixedErrors = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("error"));
-            NumberOfMixedErrors += splitedContent.Count(x => x.ToLower().Contains("info") && x.ToLower().Contains("error"));
-            NumberOfMixedErrors += splitedContent.Count(x => x.ToLower().Contains("info") && x.ToLower().Contains("warn"));
-            NumberOfSqlErrors += splitedContent.Count(x => x.ToLower().Contains("error") && x.ToLower().Contains("warn"));
-        }
-        public void CheckParametersOfTheProvidedLog(string[] splitedContent)
-        {
-            NumberOfTheLines = splitedContent.Length;
-            NumberOfInfos = splitedContent.Count(x => x.ToLower().Contains("info"));
-            NumberOfWarns = splitedContent.Count(x => x.ToLower().Contains("warn"));
-            NumberOfErrors = splitedContent.Count(x => x.ToLower().Contains("error"));
-            NumberOfSolrErrors = splitedContent.Count(x => x.ToLower().Contains("error") && x.ToLower().Contains("solr"));
-            NumberOfSolrWarns = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("solr"));
-            NumberOfDIWarnings = splitedContent.Count(x => x.ToLower().Contains("warn") && x.ToLower().Contains("constructor"));
-        }
-
         public List<string> SplitLogIntoPieces(string fileContent)
         {
             string[] splitedContent = fileContent.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
@@ -60,22 +23,17 @@ namespace LogViewer
             List<string> listOfSpecialStrings = null;
             if (strings.Contains("warn") || strings.Contains("error"))
             {
-                listOfSpecialStrings = new List<string>();
-                for (int i = 0; i < listOfLogs.Count; i++)
-                {
-                    string hardlog = listOfLogs[i].Substring(0, listOfLogs[i].IndexOf("\r\n"));
-               
-                    if (hardlog.Contains(strings[0].ToUpper()))
-                        listOfSpecialStrings.Add(listOfLogs[i]);
-                    
-                }
+                listOfSpecialStrings = CheckHeaderOfLog(strings[0], listOfLogs);
                 strings[0] = String.Empty;
-            }
-            if (listOfSpecialStrings != null)
                 return listOfSpecialStrings.Where(x => strings.All(str => x.ToLower().Contains(str.ToLower()))).ToList();
-
+            }
 
             return listOfLogs.Where(x => strings.All(str => x.ToLower().Contains(str.ToLower()))).ToList();
+        }
+
+        private List<string> CheckHeaderOfLog(string typeOfLog, List<string> listOfLogs)
+        {
+            return listOfLogs.Where(x => x.Substring(0, x.IndexOf("\r\n")).Contains(typeOfLog.ToUpper())).ToList();
         }
 
         private List<string> MakeListOfLogs(string[] splitedContent)
@@ -84,7 +42,7 @@ namespace LogViewer
             List<string> listOfLogs = new List<string>();
             foreach (string str in splitedContent)
             {
-                if ((str.Length >= 3 && int.TryParse(str.Substring(0, 2), out int s) || str.IndexOf("ManagedPoolThread") == 0  || str.IndexOf("Rebus") == 0) && !String.IsNullOrEmpty(sb.ToString()))
+                if ((str.Length >= 3 && int.TryParse(str.Substring(0, 2), out int s) || str.IndexOf("ManagedPoolThread") == 0 || str.IndexOf("Rebus") == 0) && !String.IsNullOrEmpty(sb.ToString()))
                 {
                     listOfLogs.Add(sb.ToString());
                     sb.Clear();
@@ -93,7 +51,7 @@ namespace LogViewer
                 else
                 {
                     sb.AppendLine(str);
-                    if(!sb.ToString().Contains("\r\n"))
+                    if (!sb.ToString().Contains("\r\n"))
                         sb.AppendLine("\r\n");
                 }
             }
@@ -106,7 +64,7 @@ namespace LogViewer
             return listOfLogs?.Where(
                  str => ((str.Contains("ERROR"))
                  || (str.Contains("WARN") && !str.Contains("ManagedPoolThread"))
-               //  || str.Contains("INFO")) && !str.Contains("ManagedPoolThread"))
+                 //  || str.Contains("INFO")) && !str.Contains("ManagedPoolThread"))
                  )).ToList();
         }
 
@@ -116,12 +74,12 @@ namespace LogViewer
             int i = 0;
             foreach (string str in splitedContent)
             {
-                
+
                 if (str.StartsWith(exclude))
                     ExcldudeLine = true;
 
                 else if (String.IsNullOrEmpty(str.Trim()) || str.Equals("\t"))
-                   ExcldudeLine = false;
+                    ExcldudeLine = false;
 
                 if (ExcldudeLine)
                     splitedContent[i] = "";
@@ -132,16 +90,6 @@ namespace LogViewer
 
         internal List<string> WithExcluding(string[] strings, List<string> list)
         {
-            //foreach(string str in list)
-            //{
-            //    foreach(string exclude in strings)
-            //    {
-            //        if (str.ToLower().Contains(exclude))
-            //        {
-
-            //        }
-            //    }
-            //}
             return list.Where(x => strings.All(str => !x.ToLower().Contains(str.ToLower()))).ToList();
         }
     }
