46cafd449c884253761d97a61115b2c6422a72e9
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-03-21 22:48:03 +0100

    master new interface

---
+++ LogViewer/Services/IInfoLogs.cs
@@ -0,0 +1,17 @@
+﻿namespace LogViewer
+{
+    public interface IInfoLogs
+    {
+       void CheckParametersOfTheProvidedLog(string fileContent);
+       void CheckParametersOfTheProvidedLog(string[] splitedContent);
+         int NumberOfTheLines { get; set; }
+         int NumberOfErrors { get; set; }
+         int NumberOfSolrErrors { get; set; }
+         int NumberOfSolrWarns { get; set; }
+         int NumberOfWarns { get; set; }
+         int NumberOfInfos { get; set; }
+         int NumberOfDIWarnings { get; set; }
+         int NumberOfMixedErrors { get; set; }
+         int NumberOfSqlErrors { get; set; }
+    }
+}
